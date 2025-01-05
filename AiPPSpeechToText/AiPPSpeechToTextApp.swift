60d1c4b34c23933b18b19cd0199a7ae189d680cd
dgmolina-ti <daniel.molina@flashapp.com.br>
2025-01-05 07:08:07 -0300

    Initial Commit

---
+++ AiPPSpeechToText/AiPPSpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiPPSpeechToTextApp.swift
+//  AiPPSpeechToText
+//
+//  Created by Daniel Molina on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AiPPSpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiPPSpeechToTextTests/AiPPSpeechToTextTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiPPSpeechToTextTests.swift
+//  AiPPSpeechToTextTests
+//
+//  Created by Daniel Molina on 05/01/25.
+//
+
+import Testing
+@testable import AiPPSpeechToText
+
+struct AiPPSpeechToTextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
